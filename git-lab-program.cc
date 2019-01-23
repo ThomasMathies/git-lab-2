8346dc1aefa73ec11294ebdcdb822a7defdb8848
tm318516 <tm318516@ohio.edu>
2019-01-22 19:13:09 -0500

    Enter a message here

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Thomas Mathies
+ *        Date: 1/22/2019
+ * Description: sample program to test git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
